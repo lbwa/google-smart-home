1d556da614f6070ad635f8d313a7f45c62bc74e6
lbwa <mr_lbw@outlook.com>
2019-06-08 19:59:24 +0800

    fix(smarthome): correct onSync response.payload.devices.traits synatax
    traits should be like `action.devices.traits.XXX`
    
    ref: https://developers.google.com/actions/smarthome/traits/

--- functions/src/smart-home/index.ts
+++ functions/src/smart-home/index.ts
@@ -9,28 +9,89 @@ const app = smarthome({
   key: '<api-key>'
 })
 
-app.onSync(body => {
+/**
+ * @description SYNC intents
+ * DOC: https://developers.google.com/actions/smarthome/develop/process-intents#SYNC
+ */
+app.onSync((body, headers) => {
   // TODO Implement full SYNC response
+
+  /**
+   * @description SYNC response format:
+   * https://developers.google.com/actions/smarthome/develop/process-intents#agentUserId
+   */
   return {
-    requestId: body.requestId,
+    requestId: body.requestId, // Id of request for ease of tracing
     payload: {
+      /**
+       * @description agentUserId
+       * Reﬂects the unique (and immutable) user ID on the agent's platform.
+       * The string is opaque to Google, so if there's an immutable form vs a
+       * mutable form on the agent side, use the immutable form (e.g. an
+       * account number rather than email).
+       */
       agentUserId: 'agent_user_id',
       devices: [
+        /**
+         * @description Wash guide
+         * DOC: https://developers.google.com/actions/smarthome/guides/washer
+         */
         {
+          /**
+           * @description id
+           * the id of device in the partner's cloud, immutable and unique
+           */
           id: 'washer',
-          type: 'actions.devices.types.WASHER',
-          traits: ['actions.devices.traits.OnOff'],
-          willReportState: true,
+          /**
+           * @description type
+           * The hardware type of device
+           * All types:
+           * https://developers.google.com/actions/smarthome/guides/
+           *
+           */
+          type: 'action.devices.types.WASHER',
+          /**
+           * @description traits
+           * List of traits this device supports, This deﬁnes the commands,
+           * attributes, and states that the device has.
+           */
+          traits: [
+            'action.devices.traits.OnOff',
+            'action.devices.traits.StartStop',
+            'action.devices.traits.RunCycle'
+          ],
           name: {
+            /**
+             * Optional, List of names provided by the partner rather than the
+             * user, often manufacturer names.
+             */
             defaultNames: ['Washer sample'],
+            // Required
+            /**
+             * Required. primary name of the device, generally provided by user.
+             * This is also the name the Assistant will prefer to describe the
+             * device in responses.
+             */
             name: 'Washer',
+            // Optional. Additional names provided by the user for the device.
             nicknames: ['Washer']
           },
+          /**
+           * @description willReportState
+           *  Indicates whether this device will have its states updated by the
+           * Real Time Feed.
+           * true: real-time feed for reporting state
+           * false: polling model
+           */
+          willReportState: false,
           deviceInfo: {
             manufacturer: 'Acme Co',
             model: 'acme-washer',
-            hwVersion: '1.0',
-            swVersion: '1.0.1'
+            hwVersion: '1.0', // hardware version
+            swVersion: '1.0.1' // software version
+          },
+          attributes: {
+            pauseable: true
           }
         }
       ]
