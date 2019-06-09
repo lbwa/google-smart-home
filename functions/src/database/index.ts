5bff8e1f5c566c3fda7f848ef795f8c12e88e6bd
lbwa <mr_lbw@outlook.com>
2019-06-09 23:57:04 +0800

    feat(firebase): add service account key for firebase admin sdk

--- functions/src/database/index.ts
+++ functions/src/database/index.ts
@@ -1,4 +1,18 @@
-import admin from '../admin'
+import * as admin from 'firebase-admin'
+
+let serviceAccountKey
+try {
+  serviceAccountKey = require('../firebase-admin-sdk.json')
+} catch (err) {
+  console.warn(err)
+  console.warn('Service account key is not found.')
+  console.warn('Report state and Request sync will be unavailable.')
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+  databaseURL: 'https://smart-home-88eea.firebaseio.com'
+})
 
 export const firebaseRef = admin.database().ref('/')
 
