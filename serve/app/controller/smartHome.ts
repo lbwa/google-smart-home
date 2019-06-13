46a965f4a91a77ad86afd1e9435154697c5d9ba6
lbwa <mr_lbw@outlook.com>
2019-06-13 09:28:56 +0800

    feat(serve): handle no request body when smartHome request

--- serve/app/controller/smartHome.ts
+++ serve/app/controller/smartHome.ts
@@ -3,10 +3,11 @@ import { Controller } from 'egg'
 export default class SmartHomeController extends Controller {
   public async index() {
     const { ctx, service } = this
-    const { requestId, inputs } = ctx.request.body
-    const { intent, payload = {} } = inputs[0]
 
     try {
+      const { requestId, inputs } = ctx.request.body
+      const { intent, payload = {} } = inputs[0]
+
       ctx.body = await service.smartHome.dispatch({
         type: intent,
         payload,
@@ -14,7 +15,10 @@ export default class SmartHomeController extends Controller {
       })
     } catch (err) {
       ctx.status = err.code || 400
-      ctx.body = err.msg || 'Bad request'
+      ctx.body = {
+        code: err.code || 400,
+        message: 'Bad request'
+      }
     }
   }
 }
