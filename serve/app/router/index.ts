3bd7596d5d0a07523c10bef85082630b9e022ed1
lbwa <mr_lbw@outlook.com>
2019-06-17 23:13:04 +0800

    feat(serve/auth): implement auth code endpoint for exchanging token

--- serve/app/router/index.ts
+++ serve/app/router/index.ts
@@ -3,6 +3,19 @@ import { Application } from 'egg'
 export default (app: Application) => {
   const { controller, router } = app
 
+  /**
+   * @description Authorization code endpoint of OAuth 2.0
+   */
+  router.get('/auth', controller.oauth.authCode)
+  /**
+   * @description Token exchange endpoint of OAuth 2.0
+   */
+  router.post('/token', controller.oauth.token)
+
+  /**
+   * @description Smart Home fulfillment which is used to handle Google smart
+   * home intents
+   */
   router.post('/user/create', controller.user.createUser)
   router.post('/smart_home', controller.smartHome.index)
 }
--- serve/typings/app/controller/index.d.ts
+++ serve/typings/app/controller/index.d.ts
@@ -2,11 +2,13 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportOauth from '../../../app/controller/oauth';
 import ExportSmartHome from '../../../app/controller/smartHome';
 import ExportUser from '../../../app/controller/user';
 
 declare module 'egg' {
   interface IController {
+    oauth: ExportOauth;
     smartHome: ExportSmartHome;
     user: ExportUser;
   }
