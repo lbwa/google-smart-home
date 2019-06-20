a5e86bcedd7e3ed798243668a7226acabd2c4264
lbwa <mr_lbw@outlook.com>
2019-06-20 17:34:25 +0800

    feat(smartHome): add the response of SMH data type

--- serve/app/service/SmartHome.ts
+++ serve/app/service/SmartHome.ts
@@ -1,12 +1,21 @@
 import { Service } from 'egg'
+import { DeviceState, IsDeviceOnlineState } from './Mongo'
 
 export default class SmartHome extends Service {
-  async dispatch({ type, payload, requestId }) {
+  async dispatch({
+    type,
+    payload,
+    requestId
+  }: {
+    type: string
+    payload: any
+    requestId: string
+  }) {
     if (type === 'action.devices.SYNC') {
       return this.service.smartHome.onSync()
     }
     if (type === 'action.devices.QUERY') {
-      return this.service.smartHome.onQuery(requestId, payload)
+      return this.service.smartHome.onQuery(requestId, payload.devices)
     }
     if (type === 'action.devices.EXECUTE') {
       return this.service.smartHome.onExecute(requestId, payload)
@@ -18,13 +27,17 @@ export default class SmartHome extends Service {
     return this.errorHandler('notSupported')
   }
 
-  public async onSync() {
+  /**
+   * @description Handle action.devices.SYNC.
+   * @doc https://developers.google.com/actions/smarthome/develop/process-intents#SYNC
+   */
+  async onSync(): Promise<SmartHomeResponse<SyncResponse>> {
     try {
       const userId = await this.service.auth.getUserOrThrow()
       const devices = await this.service.mongo.getDevices(userId)
 
       return {
-        requestId: this.ctx.request.body.requestId,
+        requestId: this.ctx.request.body.requestId as string,
         payload: {
           agentUserId: userId,
           devices
@@ -35,7 +48,51 @@ export default class SmartHome extends Service {
     }
   }
 
-  public async onQuery(requestId: string, payload: any) {
+  /**
+   * @description Handle action.devices.QUERY.
+   * This intent queries for the current states of devices
+   * It is used for queries where truly real-time accuracy is required
+   * (for example, the status of a door lock).
+   * @doc https://developers.google.com/actions/smarthome/develop/process-intents#QUERY
+   */
+  async onQuery(
+    requestId: string,
+    devices: DeviceState[]
+  ): Promise<SmartHomeResponse<QueryResponse>> {
+    try {
+      const userId = await this.service.auth.getUserOrThrow()
+      const queries = await Promise.all(
+        devices.map(device =>
+          this.service.mongo.queryDevices(userId, device.id)
+        )
+      )
+
+      if (!queries.length) throw new Error()
+
+      const stateMap = queries.reduce(
+        (stateMap, query) => {
+          stateMap.deviceId = query
+          return stateMap
+        },
+        {} as QueryResponse['devices']
+      )
+      return {
+        requestId,
+        payload: {
+          devices: stateMap
+        }
+      }
+    } catch (err) {
+      this.logger.error(err.message || (err as Error))
+      return Promise.reject(err.message || (err as Error))
+    }
+  }
+
+  /**
+   * @description
+   * @doc https://developers.google.com/actions/smarthome/develop/process-intents#EXECUTE
+   */
+  async onExecute(requestId: string, payload: any) {
     // todo
     return {
       requestId,
@@ -43,7 +100,11 @@ export default class SmartHome extends Service {
     }
   }
 
-  public async onExecute(requestId: string, payload: any) {
+  /**
+   * @description
+   * @doc https://developers.google.com/actions/smarthome/develop/process-intents#DISCONNECT
+   */
+  async onDisconnect(requestId: string, payload: any) {
     // todo
     return {
       requestId,
@@ -51,15 +112,7 @@ export default class SmartHome extends Service {
     }
   }
 
-  public async onDisconnect(requestId: string, payload: any) {
-    // todo
-    return {
-      requestId,
-      payload
-    }
-  }
-
-  public requestSync(agentUserId: string) {
+  requestSync(agentUserId: string) {
     // todo
     console.log('agentUserId', agentUserId)
     return {
@@ -67,7 +120,7 @@ export default class SmartHome extends Service {
     }
   }
 
-  public reportState(reportedState: any) {
+  reportState(reportedState: any) {
     // todo
     console.log('reportedState :', reportedState)
     return {
@@ -75,12 +128,35 @@ export default class SmartHome extends Service {
     }
   }
 
-  private errorHandler(msg: string) {
+  private errorHandler(msg: string): SmartHomeResponse<SmartHomeErrorResponse> {
     return {
       requestId: this.ctx.request.body.requestId,
       payload: {
         errorCode: msg || 'unknownError'
       }
     }
+  }
+}
+
+interface SmartHomeResponse<T> {
+  requestId: string
+  payload: T
+}
+
+interface SmartHomeErrorResponse {
+  errorCode: string
+  debugString?: string
+}
+
+type SyncResponse =
+  | {
+      agentUserId: string
+      devices: DeviceState[]
+    }
+  | SmartHomeErrorResponse
+
+interface QueryResponse {
+  devices: {
+    [deviceId: string]: IsDeviceOnlineState | SmartHomeErrorResponse
   }
 }
